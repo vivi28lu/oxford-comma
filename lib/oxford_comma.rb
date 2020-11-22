c1a8e2b312290bf854f9384b2a14a227d60fa249
viviana <vivi28lu@gmail.com>
2020-11-21 20:45:13 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
-
+    if array.size == 1
+        return array[0]
+    elsif array.size == 2
+        return array.join(" and ")
+    elsif array.size == 3
+        array.pop
+        array << "and starfruit"
+        return array.join ", "
+    elsif array.size > 3
+        last_fruit = array[-1]
+        array.pop
+        array << "and #{last_fruit}"
+        return array.join ", "
+    end
 end
